f307c92d1d85b9af695f9089790a21ecfc135c6b
GovindSharma0708 <146430357+GovindSharma0708@users.noreply.github.com>
2024-10-06 02:19:19 +0530

    loader updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,23 @@
-import { InfinitySpin } from 'react-loader-spinner'
+
+
+import { InfinitySpin, Ring } from 'react-loader-spinner';
+
 export const Loader = () => {
     return (
-        <div style={{ display:"flex" ,textAlign:"center",justifyContent:"center" }}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
             <InfinitySpin
-  visible={true}
-  
-  width="200"
-  color="#a9704d"
-  ariaLabel="infinity-spin-loading"
-  />
+                visible={true}
+                width="200"
+                color="#a9704d"
+                ariaLabel="infinity-spin-loading"
+            />
+            <Ring
+                visible={true}
+                height="80"
+                width="80"
+                color="#a9704d"
+                ariaLabel="ring-loading"
+            />
         </div>
     );
 };
-
