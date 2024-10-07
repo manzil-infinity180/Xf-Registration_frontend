449fd25bb71074c30b1819eed985055f947b6be7
GovindSharma0708 <146430357+GovindSharma0708@users.noreply.github.com>
2024-10-07 23:14:02 +0530

    header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 function Header() {
     return (
         <>
-       
+        <div className='card_container'>
         <section className='section_container_login section_signin' style={{
             marginTop:"250px",
             backgroundColor:"#1f1e1e55"
@@ -53,6 +53,7 @@ function Header() {
                 <svg xmlns="http://www.w3.org/2000/svg" style={{ margin:"0 3rem",fontSize:"1.5rem"}} viewBox="0 0 496 512" width="1em" height="1em" fill='#ffff'><path fill="#ffffff" d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
                 </a>
          </div>
+        
          <div style={{
             margin:"0 auto",
             display:"flex",
@@ -85,7 +86,7 @@ function Header() {
 
          </div>
           </section>
-         
+         </div>
          </>
     );
 }
