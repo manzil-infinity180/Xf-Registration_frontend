b0407d872d64a26377354bdd61f17fd38e291fd3
GovindSharma0708 <146430357+GovindSharma0708@users.noreply.github.com>
2024-10-09 19:48:58 +0530

    about me link updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -69,33 +69,13 @@ function Header() {
 
          }}>
 
-          <a href='https://github.com/manzil-infinity180' style={{textDecoration:"none",margin: "10px"}}><h1 className='h1_header'>About Me</h1></a>
+          <a href='rahulxf.tech' style={{textDecoration:"none",margin: "10px"}}><h1 className='h1_header'>About Me</h1></a>
           <a href="https://github.com/manzil-infinity180/Xf-Registration" style={{textDecoration:"none", margin: "10px"}}><h1 className='h1_header'>Contribute</h1></a>
            <a href="https://forms.gle/yz9WJEHkm7ahvu4o9" style={{textDecoration:"none", margin: "10px"}}><h1 className='h1_header'>Suggestion</h1></a>
 
           
 
            
-
-<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-           
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href='https://github.com/manzil-infinity180' class="hover:underline me-4 md:me-6">About Me</a>
-                </li>
-                <li>
-                    <a href="https://github.com/manzil-infinity180/Xf-Registration" class="hover:underline me-4 md:me-6">Contribute</a>
-                </li>
-                <li>
-                    <a href="https://forms.gle/yz9WJEHkm7ahvu4o9" class="hover:underline me-4 md:me-6">Suggestion</a>
-                </li>
-               
-            </ul>
-        </div>
-    </div>
-</footer>
 
 
          </div>
