e55f5474382dce3fe6ff9cc116c4a315f11c4075
GovindSharma0708 <146430357+GovindSharma0708@users.noreply.github.com>
2024-10-10 22:51:52 +0530

    ring-loader updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { InfinitySpin } from 'react-loader-spinner';
 import CustomRingLoader from './RingLoader'; 
 
 export const Loader = () => {
     return (
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-            <InfinitySpin
-                visible={true}
-                width="200"
-                color="#a9704d"
-                ariaLabel="infinity-spin-loading"
-            />
             <CustomRingLoader />
         </div>
     );
